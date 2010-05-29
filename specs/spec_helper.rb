2aad1b8028d651a42fb7955c7d942d91da2a3b17
Nicolas Santa <nicolas55ar@gmail.com>
2010-05-29 10:23:50 -0300

    fix bad merge

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-require 'rubygems'
-require File.join(File.dirname(__FILE__), '..', 'lib', 'fbgraph')
-=======
 $LOAD_PATH << File.expand_path('../../lib', __FILE__)
 require 'fbgraph'
->>>>>>> costan/master
