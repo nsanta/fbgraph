acb2ca84b2f587c90563511c03115f7632d82dae
John McAliley <john@couponshack.com>
2010-08-06 16:16:28 -0400

    added comments to show how to set up the settingslogic config file.  Will move to the documentation later.

--- app/models/facebook_graph_settings.rb
+++ app/models/facebook_graph_settings.rb
@@ -2,3 +2,17 @@ class FacebookGraphSettings < Settingslogic
     source "#{Rails.root}/config/fbgraph_settings.yml"
     namespace Rails.env
 end
+
+# TODO:remove and add to documentation
+# Place in /your_rails_app/config/fbgraph_settings.yml
+# defaults: &defaults
+#   client_id: yourclientid
+#   secret_id: yoursecretid
+# development:
+#   <<: *defaults
+#
+# test:
+#   <<: *defaults
+#
+# production:
+#   <<: *defaults
