8569fbc98a867b1d74398063d205e1cbd4865dc8
Nicolas Santa <nicolas55ar@gmail.com>
2010-08-18 14:58:43 -0300

    small fix on merge

--- app/models/facebook_graph_settings.rb
+++ app/models/facebook_graph_settings.rb
@@ -1,15 +1,10 @@
 class FacebookGraphSettings < Settingslogic
-<<<<<<< HEAD
-    source "#{Rails.root}/config/fbgraph_settings.yml"
-    namespace Rails.env
-end
-=======
-    source "#{Rails.root}/config/settings/fbgraph_settings.yml"
-    namespace Rails.env
+  source "#{Rails.root}/config/settings/fbgraph_settings.yml"
+  namespace Rails.env
 end
 
 # TODO:remove and add to documentation
-# Place in /your_rails_app/config/fbgraph_settings.yml
+# Place in /your_rails_app/config/settings/fbgraph_settings.yml
 # defaults: &defaults
 #   client_id: yourclientid
 #   secret_id: yoursecretid
@@ -21,4 +16,3 @@ end
 #
 # production:
 #   <<: *defaults
->>>>>>> 94780e6dd7c0f90ef5081c9a8d60360560cb4c42
