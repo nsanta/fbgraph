f0be4e3ebc442e444b93c5ffe656fdd4229b8e1e
Nicolas Santa <nicolas55ar@gmail.com>
2010-08-27 09:19:53 -0300

    add canvas class with decode method for signed_requests

---
+++ lib/fbgraph/canvas.rb
@@ -0,0 +1,34 @@
+module Fbgraph
+  
+  class Canvas
+    
+    class << self
+      def parse_signed_request(request)
+        encoded_sig, payload = request.split('.', 2)
+        sig = ""
+        urldecode64(encoded_sig).each_byte { |b|
+          sig << "%02x" % b
+        }
+        data = JSON.parse(urldecode64(payload))
+          if data['algorithm'].to_s.upcase != 'HMAC-SHA256'
+          raise "Bad signature algorithm: %s" % data['algorithm']
+        end
+        expected_sig = OpenSSL::HMAC.hexdigest('sha256', self.secret_id, payload)
+        if expected_sig != sig
+          raise "Bad signature"
+        end
+        data
+      end
+      
+      private
+      
+      def urldecode64(str)
+        encoded_str = str.gsub('-','+').gsub('_','/')
+        encoded_str += '=' while !(encoded_str.size % 4).zero?
+        Base64.decode64(encoded_str)
+      end
+    end  
+    
+  end
+  
+end
--- lib/fbgraph/client.rb
+++ lib/fbgraph/client.rb
@@ -32,27 +32,5 @@ module FBGraph
       OAuth2::Client.new(client_id, secret_id, :site => facebook_uri)
     end
     
-    def parse_signed_request(request)
-      
-      
-      encoded_sig, payload = request.split('.', 2)
-      
-      sig = ""
-      Base64.urldecode64(encoded_sig).each_byte { |b|
-        sig << "%02x" % b
-      }
-      data = ActiveSupport::JSON.decode Base64.urldecode64(payload)
-      
-      if data['algorithm'].to_s.upcase != 'HMAC-SHA256'
-        raise "Bad signature algorithm: %s" % data['algorithm']
-      end
-      
-      expected_sig = OpenSSL::HMAC.hexdigest('sha256', self.secret_id, payload)
-      if expected_sig != sig
-        raise "Bad signature"
-      end
-      
-      data
-    end
   end  
 end
