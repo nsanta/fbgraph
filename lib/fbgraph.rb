7a2ffbf6792f22bd1a667e5b449d70f524103e33
Nicolas Santa <nicolas55ar@gmail.com>
2010-08-27 09:34:13 -0300

    reorganization of source code

--- lib/fbgraph.rb
+++ lib/fbgraph.rb
@@ -6,28 +6,6 @@ require 'uri'
 require "base64"
 require "openssl"
 
-module FBGraph
-  @config = {}
-
-  class << self
-    def load_config(yaml_file)
-      return false unless File.exist?(yaml_file)
-      cfg = YAML.load(ERB.new(File.read(yaml_file)).result)
-      if defined? RAILS_ENV
-        cfg = cfg[RAILS_ENV]
-      end
-      @config = lambda { |key| cfg[key.to_s] }
-    end
-
-    def config
-      @config
-    end
-    
-  end
-end
-
-FBGraph.load_config("#{RAILS_ROOT}/config/fbgraph.yml")
-
 require 'fbgraph/client'
 require 'fbgraph/base'
 require 'fbgraph/authorization'
@@ -35,3 +13,24 @@ require 'fbgraph/selection'
 require 'fbgraph/search'
 require 'fbgraph/realtime'
 require 'fbgraph/canvas'
+
+module FBGraph
+  @config = {}
+
+  class << self
+    
+    def load_config(yaml_file)
+      return false unless File.exist?(yaml_file)
+      cfg = YAML::load(File.open(yaml_file))
+      if defined? Rails
+        cfg = cfg[Rails.env]
+      end
+      cfg
+    end
+
+    def config
+      @config ||= load_config(File.join(Rails.root , 'config' , 'facebook.yml')).freeze
+    end
+    
+  end
+end
