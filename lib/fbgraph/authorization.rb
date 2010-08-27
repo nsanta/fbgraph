81a894720f6143ac592efe4853f310ad9befb01b
Nicolas Santa <nicolas55ar@gmail.com>
2010-08-27 09:39:09 -0300

    added exchange sessions methods

--- lib/fbgraph/authorization.rb
+++ lib/fbgraph/authorization.rb
@@ -17,6 +17,21 @@ module FBGraph
       @client.auth = @client.oauth_client.web_server.get_access_token(code, options)
       @client.access_token = @client.auth.token
     end
+    
+    def upgrade_session!(key)
+      token = upgrade_session_keys(key).first
+      @client.access_token = token
+    end
+
+    def upgrade_session_keys(*keys)
+      tokens = @client.oauth_client.request(:get, '/oauth/exchange_sessions', {
+        :client_id     => @client.client_id,
+        :client_secret => @client.secret_id,
+        :type          => 'client_cred',
+        :sessions      => keys.flatten.join(',')
+      })
+      JSON.parse(tokens).map { |hash| hash['access_token'] if hash}
+    end
 
   end  
 end
