9b5e2d99ff827ce3093bf02e38c0cfc8246e0466
Nicolas Santa <nicolas55ar@gmail.com>
2012-01-16 18:29:18 -0300

    add basic logger

---
+++ lib/fbgraph/logger.rb
@@ -0,0 +1,16 @@
+module FBGraph
+  class Logger
+    def self.info(msg)
+      puts msg
+    end
+
+    def self.warn(msg)
+      puts msg
+    end
+
+    def self.error(msg)
+      puts msg
+    end
+
+  end
+end
