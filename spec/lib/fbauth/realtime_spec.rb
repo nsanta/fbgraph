5c30998fa0bf37f8dc4fd908d17d3786f65283e1
Peter Boling <peter.boling@gmail.com>
2012-09-15 09:05:15 -0400

    Switch to gem-release from jeweler for gem version bumping in a manner compatible with an FBGraph::VERSION constant. Proper spec environment setup (all specs passing). Switch from rcov to simplecov (Ruby 1.9 compatibility).

--- spec/lib/fbauth/realtime_spec.rb
+++ spec/lib/fbauth/realtime_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../../../spec_helper', __FILE__)
+require "spec_helper"
 
 describe FBGraph do
   describe FBGraph::Realtime do
---
+++ spec/support/fbgraph_spec_helper.rb
@@ -0,0 +1,13 @@
+module FBGraphSpecHelper
+  def expect_consumer(uri, result = '')
+    consumer = @client.consumer[uri]
+    consumer.stub!(:get).and_return(Struct.new(:body).new(result))
+    @client.consumer.stub!(:[]).with(uri).and_return(consumer)
+  end
+
+  def expect_consumer_post(uri, params = {}, result = '')
+    rest_client = @client.consumer[uri]
+    rest_client.stub!(:post).with(params).and_return(Struct.new(:body).new(result))
+    @client.consumer.stub!(:[]).with(uri).and_return(rest_client)
+  end
+end
